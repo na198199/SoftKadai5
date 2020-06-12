2d79a6b184a3bbbcc9f9aa83282a939f5c279d2d
kojimanaoko <kojimanaoko@localhost>
2020-06-12 16:09:22 +0900

    変更

---
+++ SofyKadai55/src/IntToEng.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class IntToEng {
+    // メインメソッド
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("数字を入力してください: ");
+        int input = sc.nextInt();
+        System.out.println(translateEng(input));
+    }
+//こんにちは
+    // 数値を英訳する変換するメソッド
+    static String translateEng(int n) {
+    	//一番目の人（じま）1~10までの範囲の出力r 
+    	String no = "";
+    	  switch (n) {
+    	   case 0: break;
+    	   case 1: no = "one"; break;
+    	   case 2: no = "two"; break;
+    	   case 3: no = "three"; break;
+    	   case 4: no = "four"; break;
+    	   case 5: no = "five"; break;
+    	   case 6: no = "six"; break;
+    	   case 7: no = "seven"; break;
+    	   case 8: no = "eight"; break;
+    	   case 9: no = "nine"; break;
+    	   case 10: no = "ten"; break;
+    	  }
+    	  return no;
+    }
+}
+    	 
+  	
+    
